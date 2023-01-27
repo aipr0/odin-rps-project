962c83efae01ac8093d20b0bd433342517b4675f
aipr0 <aipr0@protonmail.com>
2023-01-27 17:34:02 +0000

    Make computerChoice() return appropriate string

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
+//Function to create a random whole number between for 1-3
 function computerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+    const numberToChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(numberToChoice);
+
+    if (numberToChoice == 1) {
+        return "Rock";
+    }
+    else if (numberToChoice == 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
 }
 
 console.log(computerChoice());
+
+function playRound(playerChoice, computerChoice) {
+
+
+}
