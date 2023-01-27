f3af4d5e9ed788dce47b0ac772d7d131abb6d629
aipr0 <aipr0@protonmail.com>
2023-01-27 18:58:14 +0000

    Finish working RPS project all five rounds

--- javascript.js
+++ javascript.js
@@ -23,31 +23,73 @@ function playRound(playerChoice, computerChoice) {
 
     if (playerChoice == computerChoice) {
         console.log("It's a tie!");
+        return 2;
     }
     else if (playerChoice == "rock") {
         if (computerChoice == "paper") {
             console.log("You lose!");
+            return 0;
         }
         else {
             console.log("You win!");
+            return 1;
         }
     }
     else if (playerChoice == "paper") {
         if (computerChoice == "scissors") {
             console.log("You lose!");
+            return 0;
         }
         else {
             console.log("You win!");
+            return 1;
         }
     }
     else {
         if (computerChoice == "rock") {
             console.log("You lose!")
+            return 0;
         }
         else {
             console.log("You win!")
+            return 1;
         }
     }
 }
 
-playRound(prompt("Alright, pick a stance! Rock, paper, or scissors?"), computerChoose());
+function game() {
+
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let gameResult = playRound(prompt("Alright, pick a stance! Rock, paper, or scissors?"), computerChoose());
+        if (gameResult == 0) {
+            computerWins++;
+        }
+        else if (gameResult == 1) {
+            playerWins++;
+        }
+        else {
+            computerWins++;
+            playerWins++;
+        }
+    }
+
+    console.log(playerWins);
+    console.log(computerWins);
+
+    if (playerWins == computerWins) {
+        console.log("It's a tie!");
+    }
+    else if (playerWins < computerWins) {
+        console.log("You lose!");
+    }
+    else {
+        console.log("A winner is you : )");
+    }
+}
+
+
+game();
+
