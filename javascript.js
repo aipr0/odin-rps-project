261a42125725a75ac4206d531d267f4ad17044e0
aipr0 <aipr0@protonmail.com>
2023-01-27 18:40:00 +0000

    Complete a working version of one round of RPS

--- javascript.js
+++ javascript.js
@@ -1,22 +1,53 @@
 //Function to create a random whole number between for 1-3
-function computerChoice() {
+function computerChoose() {
     const numberToChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(numberToChoice);
+    //console.log(numberToChoice);
 
     if (numberToChoice == 1) {
-        return "Rock";
+        return "rock";
     }
     else if (numberToChoice == 2) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(computerChoice());
+//console.log(computerChoose());
 
 function playRound(playerChoice, computerChoice) {
 
+    console.log(playerChoice);
+    console.log(computerChoice);
 
+    if (playerChoice == computerChoice) {
+        console.log("It's a tie!");
+    }
+    else if (playerChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose!");
+        }
+        else {
+            console.log("You win!");
+        }
+    }
+    else if (playerChoice == "paper") {
+        if (computerChoice == "scissors") {
+            console.log("You lose!");
+        }
+        else {
+            console.log("You win!");
+        }
+    }
+    else {
+        if (computerChoice == "rock") {
+            console.log("You lose!")
+        }
+        else {
+            console.log("You win!")
+        }
+    }
 }
+
+playRound(prompt("Alright, pick a stance! Rock, paper, or scissors?"), computerChoose());
