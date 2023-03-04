bbf40a6906425e237dfd5013024c0cebcc2a4aa9
airp0 <103009672+aipr0@users.noreply.github.com>
2023-03-04 14:10:33 +0000

    Finish revisiting RPS assignment, remove event listeners after full game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+//Global values I'm using to tally scores, global because functions inside event listeners don't return anything aside the code they execute
+//So I needed global scope variables to iterate score
+let totalRoundsPlayed = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 //Function to create a random whole number between for 1-3, uses that number to determine the string value for the computer to play RPS
 function computerChoose() {
     const numberToChoice = Math.floor(Math.random() * 3) + 1;
@@ -16,90 +22,79 @@ function computerChoose() {
 //Plays a single round of RPS
 function playRound(playerChoice, computerChoice) {
 
+    totalRoundsPlayed++;
     //This will display who picked what choice
-    resultsDiv.textContent += (`${playerChoice}\n`);
-    resultsDiv.textContent += (`${computerChoice}\n`);
+    resultsDiv.textContent += (`Player picked: ${playerChoice}\n`);
+    resultsDiv.textContent += (`Your nemesis chose: ${computerChoice}\n`);
 
     //First if statement clears up the tie issue, the rest is the process of eliminating the other possibilities to evaluate
-    //The return values are used in game()'s main for loop to divy out the win tallies
-    //I used numbers instead of true/false because I wanted to tally the ties as a point for both the player and the computer
-    //So 0 is a computer win, 1 is a player win, 2 is a tie
+    //Tieing is a tally for both because everyone's a winner
     if (playerChoice == computerChoice) {
         console.log("It's a tie!");
-        return 2;
+        playerWins++;
+        computerWins++;
+        return;
     }
     else if (playerChoice == "rock") {
         if (computerChoice == "paper") {
             console.log("You lose!");
-            return 0;
+            computerWins++;
+            return;
         }
         else {
             console.log("You win!");
-            return 1;
+            playerWins++;
+            return;
         }
     }
     else if (playerChoice == "paper") {
         if (computerChoice == "scissors") {
             console.log("You lose!");
-            return 0;
+            computerWins++;
+            return;
         }
         else {
             console.log("You win!");
-            return 1;
+            playerWins++;
+            return;
         }
     }
     else {
         if (computerChoice == "rock") {
             console.log("You lose!")
-            return 0;
+            computerWins++;
+            return;
         }
         else {
             console.log("You win!")
-            return 1;
+            playerWins++;
+            return;
         }
     }
 }
 
-//Where the magic happens - playRound() gets looped five times and uses the return value to tally the wins, then decides the overall winner
-function game() {
-
-    //Declared variables for the player and computer win tallies so they can be incremented in the loop
-    let playerWins = 0;
-    let computerWins = 0;
-
-    //The main loop, iterates five times
-    for (let i = 0; i < 5; i++) {
-        //gamResult calls the playRound() function and assigns the return value to itself
-        let gameResult = playRound(prompt("Alright, pick a stance! Rock, paper, or scissors?"), computerChoose());
-
-        //Using gameResult, the return value from each round played, to determine who deserves a point and adds that point
-        if (gameResult == 0) {
-            computerWins++;
-        }
-        else if (gameResult == 1) {
-            playerWins++;
-        }
-        else {
-            computerWins++;
-            playerWins++;
-        }
-    }
-
-
-    //Just prints out the total tally for both players
-    console.log(playerWins);
-    console.log(computerWins);
-
-    //Evaluates the tallies, determines the winner
-    if (playerWins == computerWins) {
-        console.log("It's a tie!");
-    }
-    else if (playerWins < computerWins) {
-        console.log("You lose!");
+//My roundabout way of testing that the rounds of RPS played is no more than five
+function testBeforePlay(choice) {
+    if (totalRoundsPlayed < 5) {
+        return playRound(choice, computerChoose());
     }
     else {
-        console.log("A winner is you : )");
+        resultsDiv.textContent += "It's over!\n";
+        
+        if (playerWins == computerWins) {
+            resultsDiv.textContent += "It's a tie!";
+        }
+        else if (playerWins < computerWins) {
+            resultsDiv.textContent += "You lose!";
+        }
+        else {
+            resultsDiv.textContent += "A winner is you : )";
+        }
     }
+
+    selectRock.removeEventListener('click', rockEventHandler);
+    selectPaper.removeEventListener('click', paperEventHandler);
+    selectScissors.removeEventListener('click', scissorsEventHandler);
 }
 
 //Creates buttons to select choice and appends them to the page body
@@ -119,14 +114,17 @@ document.body.appendChild(selectScissors);
 const resultsDiv = document.createElement('div');
 document.body.appendChild(resultsDiv);
 
+function rockEventHandler() {
+    testBeforePlay("rock");
+}
+function paperEventHandler() {
+    testBeforePlay("paper");
+}
+function scissorsEventHandler() {
+    testBeforePlay("scissors");
+}
+
 //Second parameter must be a function, I had to use an arrow function since playRound calls a function itself. Weird order of things, I don't know exactly
-selectRock.addEventListener('click', () => playRound("rock", computerChoose()));
-selectPaper.addEventListener('click', () => playRound("paper", computerChoose()));
-selectScissors.addEventListener('click', () => playRound("scissors", computerChoose()));
-
-
-
-
-//Simply calls the main game function
-//game();
-
+selectRock.addEventListener('click', rockEventHandler);
+selectPaper.addEventListener('click', paperEventHandler);
+selectScissors.addEventListener('click', scissorsEventHandler);
