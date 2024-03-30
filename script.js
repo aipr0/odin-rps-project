e726b4642efc73099a11b204e4c95d6698e15825
rinzler <aipr0@protonmail.com>
2024-03-30 16:09:39 +0000

    Finalise game program
    I finished the project. Didn't take very long. I
    feel like I can do better though. I wouldn't say
    I rushed through this, but I wasn't really hung
    up on anything either.
    
    I do want to add some logic for when the player
    inputs an illegal value, but I think I'll get to
    that when I polish this project at a later (soon)
    date.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// when called, PC makes a choice
+// Setting this as global so both playRound and playGame can access them
+let playerWins = 0;
+let computerWins = 0;
+
+// returns player's choice
+function getPlayerChoice() {
+    return prompt("Rock, paper, scissors!", "");
+}
+
+// when called, returns PC's choice
 function getComputerChoice() {
     const choiceNum = Math.floor(Math.random() * 3);
     let choiceFinal;
@@ -14,43 +23,64 @@ function getComputerChoice() {
     return choiceFinal;
 }
 
-function gameRound(playerSelection, computerSelection) {
+// Plays a single round
+function playRound(playerSelection, computerSelection) {
+    // Standardises return values from get choice functions
     const playerVal = playerSelection.toLowerCase();
     const computerVal = computerSelection.toLowerCase();
 
-    const playerWins = "Player wins!";
-    const computerWins = "Computer wins!"
+    // Setting text as a val because I'm lazy
+    const playerWinText = "Player wins!";
+    const computerWinText = "Computer wins!"
 
     // I want to assign the values for RPS as numbers for logic used in this function. Might not be practical though
 
+    // Function will break here if it's a tie
     if(playerVal === computerVal) {
         return "It's a tie!";
     }
 
     if(playerVal === "rock") {
         if(computerVal === "paper") {
-            return computerWins;
+            computerWins++;
+            return computerWinText;
         } else if(computerVal === "scissors") {
-            return playerWins;
+            playerWins++;
+            return playerWinText;
         }
     } else if(playerVal === "paper") {
         if(computerVal === "rock") {
-            return playerWins;
+            playerWins++;
+            return playerWinText;
         } else if(computerVal === "scissors") {
-            return computerWins;
+            computerWins++;
+            return computerWinText;
         }
     } else if(playerVal === "scissors") {
         if(computerVal === "rock") {
-            return computerWins;
+            computerWins++;
+            return computerWinText;
         } else if(computerVal === "paper") {
-            return playerWins;
+            playerWins++;
+            return playerWinText;
         }
     }
 }
 
-function getPlayerChoice() {
-    return prompt("Rock, paper, scissors!", "");
+// Plays a whole game, five rounds of RPS, and determines the winner/loser
+function playGame() {
+
+    for(i = 0; i < 5; i++) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+
+    if(playerWins === computerWins) {
+        console.log("It's a tie!");
+    } else if (playerWins > computerWins) {
+        console.log("Player wins the game!");
+    } else {
+        console.log("Computer wins the game!")
+    }
 }
 
-let a = gameRound(getPlayerChoice(), getComputerChoice());
-console.log(a);
+playGame();
