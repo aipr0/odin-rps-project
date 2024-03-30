24f1e9289cba4ab68a7921a2a9b8cd321fb58281
rinzler <aipr0@protonmail.com>
2024-03-30 15:48:56 +0000

    Finalise gameRound()
    gameRound() works as intended for all possible
    outcomes of Rock, Paper, Scissors.
    
    This includes ties, and also accounts for player
    input regardless of case.
    
    However, I do need to program logic for when
    player inputs a value other than "rock"/"paper"/
    "scissors".

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function getComputerChoice() {
     const choiceNum = Math.floor(Math.random() * 3);
     let choiceFinal;
-    console.log(choiceNum);
 
     if(choiceNum === 0) {
         choiceFinal = "Rock";
@@ -12,6 +11,7 @@ function getComputerChoice() {
         choiceFinal = "Scissors";
     }
 
+    console.log(choiceFinal);
     return choiceFinal;
 }
 
@@ -19,6 +19,37 @@ function gameRound(playerSelection, computerSelection) {
     const playerVal = playerSelection.toLowerCase();
     const computerVal = computerSelection.toLowerCase();
 
+    const playerWins = "Player wins!";
+    const computerWins = "Computer wins!"
+
+    // I want to assign the values for RPS as numbers for logic used in this function. Might not be practical though
+
+    if(playerVal === computerVal) {
+        return "It's a tie!";
+    }
+
+    if(playerVal === "rock") {
+        if(computerVal === "paper") {
+            return computerWins;
+        } else if(computerVal === "scissors") {
+            return playerWins;
+        }
+    } else if(playerVal === "paper") {
+        if(computerVal === "rock") {
+            return playerWins;
+        } else if(computerVal === "scissors") {
+            return computerWins;
+        }
+    } else if(playerVal === "scissors") {
+        if(computerVal === "rock") {
+            return computerWins;
+        } else if(computerVal === "paper") {
+            return playerWins;
+        }
+    }
 }
 
-gameRound(prompt("Rock, paper, scissors!", ""), getComputerChoice());
+let a = gameRound(prompt("Rock, paper, scissors!", ""), getComputerChoice());
+console.log(a);
+
+
